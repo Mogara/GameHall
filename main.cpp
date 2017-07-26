03ac8f57841b82521387fcbdc18132e844e6a7e9
killercai <allen.ccccnm@gmail.com>
2017-07-26 17:07:35 +0800

    fix linux bug

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-#include <Qt>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+
 int main(int argc, char *argv[])
 {
+#if defined(Q_OS_OSX) || defined(Q_OS_WIN) || defined(Q_OS_ANDROID)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
