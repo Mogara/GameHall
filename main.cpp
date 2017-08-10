4e04604d05acf8735ef8893fc19edfa33d794858
William Huang <hylbegoat@gmail.com>
2017-08-10 21:28:21 +0800

    return it back to the initial status

--- main.cpp
+++ main.cpp
@@ -4,9 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-#if defined(Q_OS_OSX) || defined(Q_OS_WIN) || defined(Q_OS_ANDROID)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
